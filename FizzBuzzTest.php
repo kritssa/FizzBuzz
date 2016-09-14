13da3ac7d70428ca181d901345d02a819d130d15
Kritsanaphon <kritssa@kku.ac.th>
2016-09-14 09:41:03 +0700

    Test case should be two

--- FizzBuzzTest.php
+++ FizzBuzzTest.php
@@ -2,14 +2,33 @@
 
 class FizzBuzz
 {
-    function Count(){
+    function __construct($number)
+    {
+      $this->number = $number;
+    }
+
+    function Count()
+    {
+      if($this->number == 2)
+      {
+        return "2";
+      }
       return "1";
     }
 }
 
 class FizzBuzzTest extends PHPUnit_Framework_TestCase
 {
-    function testShouldBeOne(){
+    
+
+    function testShouldBeTwo()
+    {
+        $FizzBuzz = new FizzBuzz(2);
+        $this->assertEquals("2",$FizzBuzz->Count());
+    }
+
+    function testShouldBeOne()
+    {
         $FizzBuzz = new FizzBuzz(1);
         $this->assertEquals("1",$FizzBuzz->Count());
     }
